0d5aa945cd8a3c5c4788ccc90c1d7cac9696488d
Matthew Hao <mlqh@users.noreply.github.com>
2021-11-05 17:21:52 -0400

    .

--- Leetcode/src/PalindromeInt.java
+++ Leetcode/src/PalindromeInt.java
@@ -1,19 +1,20 @@
 public class PalindromeInt {
-    public boolean isPalindrome(int x) {
-        if(x < 0 || x != 0 && x%10 == 0) return false;
-        
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || x != 0 && x % 10 == 0)
+            return false;
+
         int flipped = 0;
-        
-        while(x > flipped) {
+
+        while (x > flipped) {
             flipped = (flipped * 10) + (x % 10);
             x /= 10;
         }
-        
-        if(flipped == x || flipped/10 == x) {
+
+        if (flipped == x || flipped / 10 == x) {
             return true;
         }
-        
+
         return false;
-        
+
     }
 }
