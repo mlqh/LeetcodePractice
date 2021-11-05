d872bf9b8b697ce05ad4e3f2f3ac8683395bbac1
Matthew Hao <mlqh@users.noreply.github.com>
2021-11-05 16:31:24 -0400

    Valid paren

--- Leetcode/src/App.java
+++ Leetcode/src/App.java
@@ -1,35 +1,6 @@
-import java.util.Stack;
-
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println(ValidParentheses.isValid("({[])"));
     }
 
-    public boolean isValid(String s) {
-        if(s.length() % 2 != 0) return false;
-        Stack<Character> open = new Stack<>();
-        String openBrackets = "({[";
-        String closeBrackets = ")}]";
-
-        for(int i = 0; i < s.length(); i++) {
-            char curr = s.charAt(i);
-            if(openBrackets.indexOf(curr) >= 0) {
-                open.push(Character.valueOf(curr));
-            } else {
-                if(open.empty()) return false;
-                
-                char temp = (char) (open.peek());
-                if(openBrackets.indexOf(temp) == closeBrackets.indexOf(curr)) {
-                    open.pop();
-                } else {
-                    return false;
-                }
-            }
-        }
-        if(!open.empty()) {
-            return false;
-        }
-
-        return true;
-    }
 }
---
+++ Leetcode/src/ValidParentheses.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+        if(s.length() % 2 != 0) return false;
+        Stack<Character> open = new Stack<>();
+        String openBrackets = "({[";
+        String closeBrackets = ")}]";
+
+        for(int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            if(openBrackets.indexOf(curr) >= 0) {
+                open.push(Character.valueOf(curr));
+            } else {
+                if(open.empty()) return false;
+                
+                char temp = (char) (open.peek());
+                if(openBrackets.indexOf(temp) == closeBrackets.indexOf(curr)) {
+                    open.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if(!open.empty()) {
+            return false;
+        }
+
+        return true;
+    }
+    
+}
+
+/**
+ * First check if string is even length
+ * Then, use a stack, push open brackets onto stack
+ * When encountering a closed bracket, check to see if matching open is on top of stack
+ */
