7c985e6db23e3d939073f0a4669b247f815e0ca0
Matthew Hao <mlqh@users.noreply.github.com>
2021-12-13 13:27:01 -0500

    add 2 questions

---
+++ Leetcode/src/LongestSameCharSubstring.java
@@ -0,0 +1,24 @@
+public class LongestSameCharSubstring {
+    class Solution {
+        public int maxPower(String s) {
+            if (s.length() == 1)
+                return 1;
+
+            char curr = s.charAt(0);
+            int maxPower = 1;
+            int currPower = maxPower;
+            for (int i = 1; i < s.length(); i++) {
+                if (s.charAt(i) == curr) {
+                    currPower++;
+                    if (currPower > maxPower) {
+                        maxPower = currPower;
+                    }
+                } else {
+                    curr = s.charAt(i);
+                    currPower = 1;
+                }
+            }
+            return maxPower;
+        }
+    }
+}
---
+++ Leetcode/src/MergeSorterdLists.java
@@ -0,0 +1,66 @@
+public class MergeSorterdLists {
+
+    // iterative solution
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            if (list1 == null)
+                return list2;
+            if (list2 == null)
+                return list1;
+
+            ListNode head = new ListNode(0);
+            ListNode pointer = head;
+
+            while (list1 != null && list2 != null) {
+                if (list1.val <= list2.val) {
+                    pointer.next = list1;
+                    list1 = list1.next;
+                } else {
+                    pointer.next = list2;
+                    list2 = list2.next;
+                }
+                pointer = pointer.next;
+            }
+
+            if (list1 != null) {
+                pointer.next = list1;
+            } else if (list2 != null) {
+                pointer.next = list2;
+            }
+
+            return head.next;
+        }
+
+        // recursive
+        public ListNode mergeTwoListsRecursive(ListNode l1, ListNode l2) {
+            if (l1 == null)
+                return l2;
+            if (l2 == null)
+                return l1;
+            if (l1.val < l2.val) {
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
+            }
+        }
+    }
+
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
