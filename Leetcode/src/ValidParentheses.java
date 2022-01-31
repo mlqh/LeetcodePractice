9752d94c4648c8346b56109b54c28c583d38d09e
Matthew Hao <mlqh@users.noreply.github.com>
2022-01-30 22:09:28 -0500

    improved performance

--- Leetcode/src/ValidParentheses.java
+++ Leetcode/src/ValidParentheses.java
@@ -3,40 +3,38 @@ import java.util.Stack;
 public class ValidParentheses {
 
     public static boolean isValid(String s) {
-        if(s.length() % 2 != 0) return false;
+        if (s.length() % 2 != 0)
+            return false;
         Stack<Character> open = new Stack<>();
         String openBrackets = "({[";
         String closeBrackets = ")}]";
 
-        for(int i = 0; i < s.length(); i++) {
-            char curr = s.charAt(i);
-            if(openBrackets.indexOf(curr) >= 0) {
-                open.push(Character.valueOf(curr));
+        for (char i : s.toCharArray()) {
+            if (openBrackets.indexOf(i) != -1) {
+                open.push(i);
             } else {
-                if(open.empty()) return false;
-                
-                char temp = (char) (open.peek());
-                if(openBrackets.indexOf(temp) == closeBrackets.indexOf(curr)) {
+                if (open.empty())
+                    return false;
+                if (openBrackets.indexOf(open.peek()) == closeBrackets.indexOf(i)) {
                     open.pop();
                 } else {
                     return false;
                 }
             }
         }
-        if(!open.empty()) {
-            return false;
-        }
-
-        return true;
+        return open.isEmpty();
     }
-    
+
 }
 
 /**
  * First check if string is even length
  * Then, use a stack, push open brackets onto stack
- * When encountering a closed bracket, check to see if matching open is on top of stack
+ * When encountering a closed bracket, check to see if matching open is on top
+ * of stack
  * 
- * Another solution: for every open bracket encountered, push matching close bracket onto stack
- * When encountering close bracket, check to see if matches top of stack, if not, or if stack empty false
+ * Another solution: for every open bracket encountered, push matching close
+ * bracket onto stack
+ * When encountering close bracket, check to see if matches top of stack, if
+ * not, or if stack empty false
  */
