cbd1e14c07231720fc23126a7dc8ba695f9c1d1f
Matthew Hao <mlqh@users.noreply.github.com>
2022-03-08 18:19:29 -0500

    N_Rooks

--- Leetcode/src/App.java
+++ Leetcode/src/App.java
@@ -2,9 +2,11 @@ public class App {
     public static void main(String[] args) {
         // System.out.println(ValidParentheses.isValid("({[])"));
         // System.out.println(PalindromeInt.isPalindrome(1234554321));
-        System.out.println(MinimumValueStepByStep.minStartValuePrefix(new int[] { -3, 2, -3, 4, 2 }));
-        System.out.println(RomanToInt.romanToInt("IV"));
-
+        // System.out.println(MinimumValueStepByStep.minStartValuePrefix(new int[] { -3,
+        // 2, -3, 4, 2 }));
+        // System.out.println(RomanToInt.romanToInt("IV"));
+        int[][] nRooksInput = { { 1, 2 }, { 2, 2 }, { 3, 3 }, { 4, 4 }, { 5, 5 }, { 6, 6 }, { 7, 7 }, { 8, 8 } };
+        System.out.println(N_Rooks.checkAttack(nRooksInput));
     }
 
 }
---
+++ Leetcode/src/N_Rooks.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class N_Rooks {
+    public static boolean checkAttack(int[][] coordinates) {
+        Set<Integer> xCoordinates = new HashSet<>();
+        Set<Integer> yCoordinates = new HashSet<>();
+        for (int i = 0; i < coordinates.length; i++) {
+            boolean overlap = xCoordinates.add(coordinates[i][0]) && yCoordinates.add(coordinates[i][1]) ? false : true;
+            if (overlap) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
