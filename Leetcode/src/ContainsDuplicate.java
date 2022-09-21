65562ac89efda9e05d8208bd0fb01e646cda0e76
Matthew Hao <mlqh@users.noreply.github.com>
2022-09-21 03:55:14 -0400

    Contains Duplicate

---
+++ Leetcode/src/ContainsDuplicate.java
@@ -0,0 +1,11 @@
+class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
