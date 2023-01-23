e344a9065f7e44a498332335f95be54d2c7f40c6
mlqh <matt.lqh02@gmail.com>
2023-01-22 21:37:36 -0500

    valid anagram

---
+++ Leetcode/src/ValidAnagram.java
@@ -0,0 +1,20 @@
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+        int[] freqCount = new int[26];
+        for(char letter: s.toCharArray()) {
+            freqCount[letter-'a']++;
+        }
+        for(char letter: t.toCharArray()) {
+            freqCount[letter-'a']--;
+        }
+        for(int i=0;i<26;i++) {
+            if(freqCount[i] != 0) {
+                return false;
+            } 
+        }
+        return true;
+    }
+}
