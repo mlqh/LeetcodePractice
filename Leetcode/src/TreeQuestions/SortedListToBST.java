ccd84429daa8c391fb181151b0ebea4994d52cbd
mlqh <matt.lqh02@gmail.com>
2023-02-03 15:39:07 -0500

    SortedListToBST

---
+++ Leetcode/src/TreeQuestions/SortedListToBST.java
@@ -0,0 +1,24 @@
+package TreeQuestions;
+
+/**
+ * Use slow fast pointers to find middle
+ * Cutoff left sub-list to have recursion stop on its own
+ */
+
+public class SortedListToBST {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head == null) return null;
+        if(head.next == null) return new TreeNode(head.val);
+        ListNode slow = head, prev = null, fast = head;
+        while(fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        TreeNode node = new TreeNode(slow.val);
+        node.left = sortedListToBST(head);
+        node.right = sortedListToBST(slow.next);
+        return node;
+    }
+}
