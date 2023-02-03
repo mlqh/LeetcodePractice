30b42dfa879a6337219d6a1944715a912ac9f7f5
mlqh <matt.lqh02@gmail.com>
2023-02-03 15:20:26 -0500

    Add some tree q

---
+++ Leetcode/src/TreeQuestions/MaxDepth.java
@@ -0,0 +1,8 @@
+package TreeQuestions;
+
+public class MaxDepth {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
