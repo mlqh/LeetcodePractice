a930c2da21bb0122d8e096370b1e60b3b6c6728d
mlqh <matt.lqh02@gmail.com>
2023-11-06 17:37:29 -0500

    Neetcode insert sort

---
+++ Neetcode/src/Practice/Basics/InsertionSort.java
@@ -0,0 +1,36 @@
+package Practice.Basics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertionSort {
+    // Definition for a pair
+    class Pair {
+        int key;
+        String value;
+    
+        Pair(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
+    public class Solution {
+        public List<List<Pair>> insertionSort(List<Pair> pairs) {
+            List<List<Pair>> state = new ArrayList<>();
+            
+            for(int i = 0; i < pairs.size(); i++) {
+                for(int j = i - 1; j >= 0 ; j--) {
+                    if(pairs.get(j + 1).key < pairs.get(j).key) {
+                        Pair temp = pairs.get(j+1);
+                        pairs.set(j+1, pairs.get(j));
+                        pairs.set(j, temp);
+                    }
+                }
+                List<Pair> newClone = new ArrayList<>(pairs);
+                state.add(newClone);
+            }
+            return state;
+        }
+    }
+}
